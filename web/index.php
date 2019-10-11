6b8cc14f6eec2c9dbd69805c595d09daf92dbd24
ronagreat <46036773+ronagreat@users.noreply.github.com>
2019-10-11 20:54:20 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,7 +1,13 @@
 <html>
-  
-  <p>10116313</p><br>
-  <a href="https://dashboard.heroku.com/apps/tugas1-10116313/"><p>Kemas Risman Fauzan</p></a>
+  <h1>Kelompok 8</h1><br>
+  <ol>
+	  <li><a href="https://tugas1-101163xx-29.herokuapp.com">Kemas Risman Fauzan</a></li>
+	  <li><a href="https://tugas1-101163xx-29.herokuapp.com">Heddy Cahya Firdaus</a></li>
+	  <li><a href="https://tugas1-101163xx-29.herokuapp.com">Muhammad Reza Muzaky</a></li>
+	  <li><a href="https://tugas1-101163xx-29.herokuapp.com">Vanadia Aviandi</a></li>
+	  <li><a href="https://tugas1-101163xx-29.herokuapp.com">Puspita Rahayu</a></li>
+	</ol><br>
+	<a href="https://tugas1-101163xx-29.herokuapp.com">Review Security Cloud Computing</a>
   
   
 </html>
