119be24fa79b058c502b32c11031333b7576c2b3
ronagreat <46036773+ronagreat@users.noreply.github.com>
2019-10-13 10:26:40 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@
   	
 	  <li><a href="https://tugas1-101163xx-29.herokuapp.com/individu/kemas.php">Kemas Risman Fauzan</a></li>
 	  <li><a href="https://tugas1-10116309.herokuapp.com/">Heddy Cahya Firdaus</a></li>
-	  <li><a href="https://tugas1-101163xx-29.herokuapp.com">Muhammad Reza Muzaky</a></li>
+	  <li><a href="https://tugas1-101162xx-17.herokuapp.com/">Muhammad Reza Muzaky</a></li>
 	  <li><a href="https://tugas1-101162xx-16.herokuapp.com/">Vanadia Aviandi</a></li>
 	  <li><a href="https://tugas1-101162xx-18.herokuapp.com/">Puspita Rahayu</a></li>
 	</ol>
